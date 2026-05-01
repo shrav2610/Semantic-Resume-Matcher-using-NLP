cd41b4e0d7af58b9b186a2d5acccc04bb13c7b4d
shrav2610 <shravani.birdawade@gmail.com>
2026-05-01 20:25:15 +0530

    Delete parser.py

--- parser.py
+++
@@ -1,8 +0,0 @@
-from pdfminer.high_level import extract_text
-
-def load_resume(path):
-    return extract_text(path)
-
-def load_job_description(path):
-    with open(path, "r", encoding="utf-8") as f:
-        return f.read()
